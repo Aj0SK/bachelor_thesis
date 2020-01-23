9daa58aa5a8c03373f4210a490c810a4a6fb5160
Aj0SK <andrejkorman@gmail.com>
2020-01-23 21:55:56 +0100

    Final changes regarding homework

--- code/helpers/mapSequence/main.py
+++ code/helpers/mapSequence/main.py
@@ -1,4 +1,8 @@
 minimalHitLength = 30
+seqLimit = 1000
+refFilePath = "../../data/sapIngB1.fa"
+readsFilePath = "../../data/reads.fastq"
+kmerModelFilePath = "../../data/kmer_model.hdf5"
 
 import sys
 import numpy as np
@@ -8,17 +12,17 @@ import h5py
 import nadavca
 from nadavca.dtw import KmerModel
 
-sequenceIndex = mp.Aligner("../../data/sapIngB1.fa")
+sequenceIndex = mp.Aligner(refFilePath)
 assert sequenceIndex, "failed to load/build reference index"
 
 hits = []
 
-with open("../../data/reads.fastq", "r") as f: 
+with open(readsFilePath, "r") as f: 
     lines = f.readlines()
     old = [lines[i+1] for i in range(len(lines)) if lines[i][0] == '@']
     counter = 0
     for seq in old:
-        if counter >= 1000:
+        if counter >= seqLimit:
             break
         refHits = [i.ctg for i in list(sequenceIndex.map(seq))]
         if len(set(refHits)) is not 1:
@@ -26,7 +30,7 @@ with open("../../data/reads.fastq", "r") as f:
         hits.append((refHits[0], seq))
         counter += 1
 
-mod = KmerModel.load_from_hdf5("../../data/kmer_model.hdf5")
+mod = KmerModel.load_from_hdf5(kmerModelFilePath)
 
 for contig, ref_str in hits:
     if len(ref_str) < minimalHitLength:
