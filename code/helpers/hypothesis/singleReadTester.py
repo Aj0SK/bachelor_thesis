110e058fec66b53b71fe4623cdd56c4e2a920d02
Aj0SK <andrejkorman@gmail.com>
2020-04-15 23:15:24 +0200

    Add helpful read grapher

--- code/helpers/hypothesis/singleReadTester.py
+++ code/helpers/hypothesis/singleReadTester.py
@@ -5,253 +5,108 @@
 # corresponding reference sequence we can try to create fake signal and see the
 # differences.
 
-import argparse
-
-parser = argparse.ArgumentParser(description='Options usable in this module.')
-parser.add_argument("-p", "--sampleRead", help="location of positive sample read", type=str, default=None)
-parser.add_argument("-n", "--sampleFakeRead", help="location of positive sample read", type=str, default = None)
-args = parser.parse_args()
-
-# here we want to chose some part of signal that is not close to beg or end of read
-refFilePath = "../../data/sapIngB1.fa"
-sampleRead = args.sampleRead
-sampleFakeRead = args.sampleFakeRead
-
-# here we want to chose some part of signal that is not close to beg or end of read
-fromSignal, toSignal = 20000, 22000
-fromSignalFake, toSignalFake = 15000, 17000
-refFilePath = "../../data/sapIngB1.fa"
-
+readsPosFilePath = "../../data/pos-basecalled"
+readsNegFilePath = "../../data/neg-basecalled"
 kmerModelFilePath = "../../data/kmer_model.hdf5"
-# number of levels we use
+refFilePath = "../../data/sapIngB1.fa"
+
+repeatSignal = 10
+'''
 numLevels = 4
 minSignal, maxSignal = -2.0, 2.0
-repeatSignal = 8
 kernelLen = 7
 winSize = 15 * kernelLen
+'''
 
 import math
 import h5py
-import copy
 import numpy as np
 import mappy as mp
 from pyfaidx import Fasta
+import nadavca
 from nadavca.dtw import KmerModel
-from signalHelper import getLevels, getLevelString, stringToSignal, normalizeWindow, getSignalFromRead, getSeqfromRead, Table_Iterator, seqSignalCor
+
+from signalHelper import stringToSignal, getSignalFromRead, getReadsInFolder
+from signalHelper import computeNorm, computeString, smoothSignal, buildDictionary, overlappingKmers
 
 import matplotlib
-# this allows us to see graphs through ssh
-matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
-plt.close('all')
 
-############s####################################################################
+############s###################################################################
+
+posReadsPaths = getReadsInFolder(readsPosFilePath, minSize = 0)
+negReadsPaths = getReadsInFolder(readsNegFilePath, minSize = 0)
+
+sampleRead = posReadsPaths[0]
+sampleFakeRead = negReadsPaths[0]
+
+############s###################################################################
 # load reference sequence and create index for fast mapping
-
 ref = Fasta(refFilePath)
 
-sequenceIndex = mp.Aligner(refFilePath)
-assert sequenceIndex, "failed to load/build reference index"
-
-################################################################################
 # load basecalled sequence and signal
 mod = KmerModel.load_from_hdf5(kmerModelFilePath)
 
-# read fastq from file
-fastqSeq, basecallTable = getSeqfromRead(sampleRead)
-# read second line and decode it into string from bytes
+sequenceIndex = mp.Aligner(refFilePath)
+assert sequenceIndex, "failed to load/build reference index"
+################################################################################
+# nadavca
+nadavca_align = nadavca.align_signal(refFilePath, [sampleRead], bwa_executable='./bwa/bwa')
+
+assert (len(nadavca_align) == 1), "Error! More than one alignment!"
+nadavca_align = nadavca_align[0]
+assert (nadavca_align[0].reverse_complement == False), "Error! Reverse strand!"
+################################################################################
+
+refStr = str(ref[nadavca_align[0].contig_name])
+fromSignal, toSignal = nadavca_align[0].signal_range
 
 # load original signal from read
 originalSignal = getSignalFromRead(sampleRead)
 originalSignal = np.array(originalSignal, dtype = float)
 
-################################################################################
-# get only part of signal and find corresponding basecalled sequence, then find
-# it in reference
-signalFrTo = seqSignalCor(fromSignal, toSignal, basecallTable)
+table = nadavca_align[1][:100]
+refSeq = "".join(nadavca_align[0].reference_part)[:100]
 
-hits = [i for i in sequenceIndex.map(signalFrTo) if i.q_en - i.q_st > 95*len(signalFrTo)//100]
-if len(hits) != 1:
-    print("Too many or too few hits, skipping read.")
-    exit(0)
-else:
-    print("Mapped from {0} to {1} from {2}".format(hits[0].q_st, hits[0].q_en, len(signalFrTo)))
+x, y = [], []
 
-signalFrTo = str(ref[hits[0].ctg][hits[0].r_st:hits[0].r_en])
+for entry in table:#entry is list of [ref_index, signal_start, signal_end]
+    x.append(str(refStr[entry[0]]))
+    for i in range(entry[1], entry[2]):
+        x.append(" ")
+        y.append(originalSignal[i])
 
-# create artificial signal
-artifSignal = np.array(stringToSignal(signalFrTo, mod, repeatSignal = repeatSignal), float)
-originalSignal = originalSignal[fromSignal:toSignal]
+#plt.plot(y)
+#plt.xticks(y_pos, x, color='orange', rotation=45, fontweight='bold', horizontalalignment='right')
+#plt.tick_params(labelbottom='off')
 
-# load some fake signal
-fakeSignal = getSignalFromRead(sampleFakeRead)
-fakeSignal = np.array(fakeSignal, dtype = float)
-fakeSignal = fakeSignal[fromSignalFake:toSignalFake]
+refSignal = stringToSignal(refSeq, mod, repeatSignal=repeatSignal)
+refSeqHelper = []
 
-################################################################################
-# get levels
+for i in refSeq:
+    refSeqHelper.append(i)
+    for k in range(repeatSignal-1):
+        refSeqHelper.append("_")
 
-levelO = getLevels(originalSignal, kernelLen = kernelLen, numLevels = numLevels, minSignal = minSignal, maxSignal = maxSignal)
+y = smoothSignal(y, 5)
+refSignal = smoothSignal(refSignal, 5)
 
-levelA = getLevels(artifSignal, kernelLen = kernelLen, numLevels = numLevels, minSignal = minSignal, maxSignal = maxSignal)
+ySignalShift, ySignalScale = computeNorm(y, 0, len(y))
+y -= ySignalShift
+y /= ySignalScale
 
-# count have many strings are shared by fabricatedSignal and original/processedSignal
-counterO = len([i for i in levelO if i in levelA])
-print("Artificial match rate with original signal {0}/{1}".format(counterO, len(levelO)))
+refSignalShift, refSignalScale = computeNorm(refSignal, 0, len(refSignal))
+refSignal -= refSignalShift
+refSignal /= refSignalScale
 
-# load original signal from read
-fakeSignal = getSignalFromRead(sampleFakeRead)
-fakeSignal = np.array(fakeSignal, dtype = float)
-fakeSignal = fakeSignal[fromSignalFake:toSignalFake]
-levelF = getLevels(fakeSignal, kernelLen = kernelLen, numLevels = numLevels, minSignal = minSignal, maxSignal = maxSignal)
+fig, axs = plt.subplots(2)
+fig.suptitle('Read vs reference')
+axs[0].plot(y)
+axs[0].set_xticks(np.arange(len(x)))
+axs[0].set_xticklabels(x)
 
-counterF = len([i for i in levelF if i in levelA])
-print("Total fake match rate with original signal {0}/{1}".format(counterF, len(levelF)))
-
-################################################################################
-#
-
-counter = 0
-for w in levelO:
-    for i in range(kernelLen-1):
-        if abs(ord(w[i])-ord(w[i+1])) > 1:
-            counter += 1
-
-print("Jumps {0}/{1}".format(counter, len(levelO)*kernelLen))
-
-counter = 0
-for w in levelF:
-    for i in range(kernelLen-1):
-        if abs(ord(w[i])-ord(w[i+1])) > 1:
-            counter += 1
-
-print("Jumps {0}/{1}".format(counter, len(levelF)*kernelLen))
-
-################################################################################
-#
-
-'''
-levelStringStarts = {}
-for winBeg in range(0, originalSignal.shape[0]-winSize, 100):
-        winEnd = winBeg + winSize
-        currWindow = copy.deepcopy(originalSignal[winBeg:winEnd])
-        normalizeWindow(currWindow, minSignal, maxSignal)
-        outString = getLevelString(currWindow, minSignal, maxSignal, numLevels)
-        if len(outString) < kernelLen:
-            continue
-        outString = outString[:kernelLen]
-        levelStringStarts[outString] = winBeg
-
-fastq1, basecallTable1 = getSeqfromRead(sampleRead)
-fastq2, basecallTable2 = getSeqfromRead(sampleFakeRead)
-
-for winBeg in range(0, artifSignal.shape[0]-winSize, 1):
-        winEnd = winBeg + winSize
-
-        # normalize window
-        currWindow = copy.deepcopy(artifSignal[winBeg:winEnd])
-        normalizeWindow(currWindow, minSignal, maxSignal)
-
-        # cut into horizontal levels
-        outString = getLevelString(currWindow, minSignal, maxSignal, numLevels)
-        if len(outString) < kernelLen:
-            continue
-        outString = outString[:kernelLen]
-        if outString in levelStringStarts:
-            print("Zhoda: {0} {1}".format(winBeg, levelStringStarts[outString]))
-            print(outString)
-            #f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-            sig1 = copy.deepcopy(originalSignal[levelStringStarts[outString]:levelStringStarts[outString]+winSize])
-            sig2 = copy.deepcopy(artifSignal[winBeg:winEnd])
-            normalizeWindow(sig1)
-            normalizeWindow(sig2)
-            #l1, = ax1.plot(np.arange(winSize), sig1, lw=2)
-            #l2, = ax2.plot(np.arange(winSize), sig2, lw=2)
-            #ax1.set_ylim([minSignal-0.1, maxSignal+0.1])
-            #ax2.set_ylim([minSignal-0.1, maxSignal+0.1])
-            #ax1.hlines(y=[minSignal+i*((maxSignal-minSignal)/numLevels) for i in range(numLevels+1)], xmin=0, xmax=winSize, linewidth=1, color='gray')
-            #ax2.hlines(y=[minSignal+i*((maxSignal-minSignal)/numLevels) for i in range(numLevels+1)], xmin=0, xmax=winSize, linewidth=1, color='gray')
-            #plt.show()
-            signalFrTo1 = seqSignalCor(levelStringStarts[outString], levelStringStarts[outString]+winSize, basecallTable1)
-            signalFrTo2 = seqSignalCor(winBeg, winEnd, basecallTable2)
-            print(signalFrTo1)
-            print(signalFrTo2)
-
-'''
-        
-
-################################################################################
-# Graph normalized original, processed and artificial signal
-
-#normalizeWindow(originalSignal)
-#normalizeWindow(artifSignal)
-#normalizeWindow(fakeSignal)
-
-o = np.linspace(0, originalSignal.shape[0], originalSignal.shape[0])
-a = np.linspace(0, artifSignal.shape[0], artifSignal.shape[0])
-f = np.linspace(0, fakeSignal.shape[0], fakeSignal.shape[0])
-
-fig, axs = plt.subplots(3)
-fig.suptitle('Original, modified original(processed) and artificial signal')
-axs[0].plot(o, originalSignal, label='data 1', color = 'r')
-axs[1].plot(f, fakeSignal, label='data 2', color = 'g')
-axs[2].plot(a, artifSignal, label='data 3', color = 'b')
-
-axs[0].hlines(y=[minSignal+i*((maxSignal-minSignal)/numLevels) for i in range(numLevels+1)], xmin=0, xmax=len(o), linewidth=1, color='gray')
-axs[2].hlines(y=[minSignal+i*((maxSignal-minSignal)/numLevels) for i in range(numLevels+1)], xmin=0, xmax=len(a), linewidth=1, color='gray')
-plt.show(block=False)
-
-from matplotlib.widgets import Slider
-import edlib
-
-f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-plt.subplots_adjust(left=0.25, bottom=0.25)
-l1, = ax1.plot(np.arange(winSize), originalSignal[:winSize], lw=2)
-l2, = ax2.plot(np.arange(winSize), artifSignal[:winSize], lw=2)
-
-ax1.set_ylim([minSignal-0.1, maxSignal+0.1])
-ax2.set_ylim([minSignal-0.1, maxSignal+0.1])
-
-ax1.hlines(y=[minSignal+i*((maxSignal-minSignal)/numLevels) for i in range(numLevels+1)], xmin=0, xmax=winSize, linewidth=1, color='gray')
-ax2.hlines(y=[minSignal+i*((maxSignal-minSignal)/numLevels) for i in range(numLevels+1)], xmin=0, xmax=winSize, linewidth=1, color='gray')
-
-axcolor = 'lightgoldenrodyellow'
-axorig = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-axartif = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
-
-sOrig = Slider(axorig, 'Orig', 0, toSignal-fromSignal-winSize, valinit=0, valstep=1)
-sArtif = Slider(axartif, 'Artif', 0, artifSignal.shape[0]-winSize, valinit=0, valstep=1)
-
-def update(val):
-    newBeg1, newBeg2 = int(sOrig.val), int(sArtif.val)
-    origW = copy.deepcopy(originalSignal[newBeg1:(newBeg1+winSize)])
-    artifW = copy.deepcopy(artifSignal[newBeg2:(newBeg2+winSize)])
-    shiftOrigUp = copy.deepcopy(origW)
-    shiftOrigDw = copy.deepcopy(origW)
-    normalizeWindow(origW)
-    normalizeWindow(artifW)
-    normalizeWindow(shiftOrigUp, shift = 0.4)
-    normalizeWindow(shiftOrigDw, shift = -0.4)
-    l1.set_ydata(origW)
-    l2.set_ydata(artifW)
-    levelS1 = getLevelString(origW, numLevels = numLevels)
-    levelS2 = getLevelString(artifW, numLevels = numLevels)
-    levelS3 = getLevelString(shiftOrigUp, numLevels = numLevels)
-    levelS4 = getLevelString(shiftOrigDw, numLevels = numLevels)
-    # orig
-    print("O:" + levelS1)
-    # artif
-    print("A:" + levelS2)
-    # shift up
-    print("U:" + levelS3)
-    # shift down
-    print("D:" + levelS4)
-    print("Distance of artif from orig is {0}".format(edlib.align(levelS1, levelS2)["editDistance"]))
-    print("Distance of shiftedUp from orig is {0}".format(edlib.align(levelS3, levelS2)["editDistance"]))
-    print("Distance of shiftedDw from orig is {0}".format(edlib.align(levelS4, levelS2)["editDistance"]))
-    fig.canvas.draw_idle()
-
-sOrig.on_changed(update)
-sArtif.on_changed(update)
+axs[1].plot(refSignal)
+axs[1].set_xticks(np.arange(len(refSeqHelper)))
+axs[1].set_xticklabels(refSeqHelper)
 
 plt.show()
